f2f38bd4deae093b6a3ed9bc437ac3bdb78169c4
Shashi Kiran Perka <shashikira4124@gmail.com>
2026-05-07 19:26:50 +0530

    Replace nedb with @seald-io/nedb for Node.js v24 compatibility

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
-const nedb = require('nedb');
-const users = new nedb({ filename: 'db/users.db', autoload: true })
-const products = new nedb({ filename: 'db/products.db', autoload: true });
+const Datastore = require('@seald-io/nedb');
+const users = new Datastore({ filename: 'db/users.db', autoload: true })
+const products = new Datastore({ filename: 'db/products.db', autoload: true });
 
 module.exports.users = users
 module.exports.products = products
